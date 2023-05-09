bbea9b0579f1eca0939b69e6e800c0a4ab2aefda
Tabing010102 <tabing010102@outlook.com>
2023-05-09 20:10:54 +0800

    vs WPF-UI compact navigation init

---
+++ Waiter/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Waiter.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
