79291f527abeddf3af8dbd35ec8b851fb3960247
Tabing010102 <tabing010102@outlook.com>
2023-05-25 10:56:14 +0800

    add AppBaseDir migration

---
+++ Waiter/Migrations/20230525025602_AddAppBaseDir.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Waiter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAppBaseDir : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WorkDir",
+                table: "AppPackageSettings",
+                newName: "AppWorkDir");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AppBaseDir",
+                table: "AppPackageSettings",
+                type: "TEXT",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AppBaseDir",
+                table: "AppPackageSettings");
+
+            migrationBuilder.RenameColumn(
+                name: "AppWorkDir",
+                table: "AppPackageSettings",
+                newName: "WorkDir");
+        }
+    }
+}
