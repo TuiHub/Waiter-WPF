ee0d4d9ae65ba0cf8fda6b737eee60ffaaefffcc
Tabing010102 <tabing010102@outlook.com>
2023-07-04 11:10:58 +0800

    add SizeBytesToHRStringHelper

--- Waiter/Helpers/SizeBytesToHRStringConverter.cs
+++ Waiter/Helpers/SizeBytesToHRStringConverter.cs
@@ -14,14 +14,7 @@ namespace Waiter.Helpers
     {
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            // https://stackoverflow.com/a/4975942
-            string[] suf = { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB" }; //Longs run out around EB
-            if ((long)value == 0)
-                return "0" + suf[0];
-            long bytes = Math.Abs((long)value);
-            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
-            double num = Math.Round(bytes / Math.Pow(1024, place), 2);
-            return (Math.Sign((long)value) * num).ToString() + " " + suf[place];
+            return SizeBytesToHRStringHelper.SizeBytesToHRString((long)value);
         }
 
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
---
+++ Waiter/Helpers/SizeBytesToHRStringHelper.cs
@@ -0,0 +1,24 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waiter.Helpers
+{
+    public static class SizeBytesToHRStringHelper
+    {
+        public static string SizeBytesToHRString(long sizeBytes)
+        {
+            // https://stackoverflow.com/a/4975942
+            string[] suf = { "B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB" }; //Longs run out around EB
+            if (sizeBytes == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(sizeBytes);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 2);
+            return (Math.Sign(sizeBytes) * num).ToString() + " " + suf[place];
+        }
+    }
+}
