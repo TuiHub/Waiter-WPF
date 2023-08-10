b07e8b2b1bdca06dc78fe33887df3e99e2542c91
Tabing010102 <tabing010102@outlook.com>
2023-08-10 20:23:37 +0800

    update RunWithEnsureLoginAsync

--- Waiter/Helpers/EnsureLoginHelper.cs
+++ Waiter/Helpers/EnsureLoginHelper.cs
@@ -17,10 +17,15 @@ namespace Waiter.Helpers
     {
         public static async Task RunWithEnsureLoginAsync(Func<Task> Work, Func<Task> WorkCleanup)
         {
+            var progressRingDialog = new ProgressRingWindow();
+            progressRingDialog.Owner = App.Current.MainWindow;
             while (true)
             {
                 try
                 {
+                    progressRingDialog.ViewModel.WorkText = "Loading...";
+                    App.Current.MainWindow.IsEnabled = false;
+                    progressRingDialog.Show();
                     await Work();
                 }
                 catch (RpcException ex) when (ex.Status.StatusCode == StatusCode.Unauthenticated)
@@ -28,26 +33,18 @@ namespace Waiter.Helpers
                     if (GlobalContext.UserConfig.IsLoggedIn == true)
                     {
                         // show progressRingDialog
-                        var progressRingDialog = new ProgressRingWindow();
-                        progressRingDialog.Owner = App.Current.MainWindow;
                         progressRingDialog.ViewModel.WorkText = "Logging in using refresh token...";
-                        progressRingDialog.Show();
-                        App.Current.MainWindow.IsEnabled = false;
                         // try refresh token
                         var client = new LibrarianSephirahService.LibrarianSephirahServiceClient(GlobalContext.GrpcChannel);
                         var (accessToken, refreshToken) = await GlobalContext.LibrarianClientService.GetTokenAsync(client);
-                        // close progressRingDialog
-                        progressRingDialog.Close();
+                        
                         // refresh token auth succeed
                         if (accessToken != null && refreshToken != null)
                         {
                             await GlobalContextStateHelper.UpdateLoginState(accessToken, refreshToken);
                             await WorkCleanup();
-                            App.Current.MainWindow.IsEnabled = true;
                             continue;
                         }
-                        // enable MainWindow, for ShowDialog will block user from interacting with parent window
-                        App.Current.MainWindow.IsEnabled = true;
                     }
 
                     // user not logged in or refresh token auth failed, show login dialog
@@ -59,6 +56,9 @@ namespace Waiter.Helpers
                     if (loginWindow.ViewModel.IsCodeClosed == false)
                     {
                         await WorkCleanup();
+                        // close progressRingDialog
+                        progressRingDialog.Close();
+                        App.Current.MainWindow.IsEnabled = true;
                         MessageBox.Show("User cancelled login.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
                         break;
                     }
@@ -69,6 +69,16 @@ namespace Waiter.Helpers
                         continue;
                     }
                 }
+                catch
+                {
+                    // close progressRingDialog
+                    progressRingDialog.Close();
+                    App.Current.MainWindow.IsEnabled = true;
+                    throw;
+                }
+                // close progressRingDialog
+                progressRingDialog.Close();
+                App.Current.MainWindow.IsEnabled = true;
                 break;
             }
         }
