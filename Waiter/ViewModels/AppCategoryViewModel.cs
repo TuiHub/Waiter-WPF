7d598ff70d6d28d0a67d6e0564653e12942aa67d
Tabing010102 <tabing010102@outlook.com>
2023-10-15 22:43:13 +0800

    add AppCategory ListView

--- Waiter/ViewModels/AppCategoryViewModel.cs
+++ Waiter/ViewModels/AppCategoryViewModel.cs
@@ -1,5 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+using Waiter.Helpers;
 using Wpf.Ui.Common.Interfaces;
 
 namespace Waiter.ViewModels
@@ -8,9 +14,26 @@ namespace Waiter.ViewModels
     {
         [ObservableProperty]
         private int _counter = 0;
+        [ObservableProperty]
+        private Core.Models.AppCategory? _selectedAppCategory;
+        [ObservableProperty]
+        private List<Core.Models.AppCategory> _appCategories = new();
 
-        public void OnNavigatedTo()
+        public async void OnNavigatedTo()
         {
+            try
+            {
+                await EnsureLoginHelper.RunWithEnsureLoginAsync(async () =>
+                {
+                    var client = new LibrarianSephirahService.LibrarianSephirahServiceClient(GlobalContext.GrpcChannel);
+                    AppCategories = (await GlobalContext.LibrarianClientService.ListAppCategoriesAsync(client)).ToList();
+                },
+                async () => { });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Caught exception {ex.GetType()}, message:\n{ex.Message}", "Runtime Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
 
         public void OnNavigatedFrom()
@@ -21,6 +44,30 @@ namespace Waiter.ViewModels
         private void OnCounterIncrement()
         {
             Counter++;
+        }
+
+        [RelayCommand]
+        private void OnShowSelectedAppCategory()
+        {
+            string appCategoryInfoStr = "";
+            if (SelectedAppCategory == null)
+                appCategoryInfoStr = "null";
+            else
+                appCategoryInfoStr = $"InternalId = {SelectedAppCategory.InternalId}, " +
+                                     $"Name = {SelectedAppCategory.Name}";
+            MessageBox.Show($"Selected AppCategory = {appCategoryInfoStr}", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        [RelayCommand]
+        private void OnEditAppCategory()
+        {
+
+        }
+
+        [RelayCommand]
+        private void OnDeleteAppCategory()
+        {
+
         }
     }
 }
